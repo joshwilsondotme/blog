8f21ffed435846f2795785410c9b2ff5053b57b5
Josh Wilson <joshwilsondesign@gmail.com>
2017-05-14 21:25:00 -0400

    Add, Edit, and Delete added to PostController

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -11,7 +11,7 @@ class PostsController extends AppController {
   }
 
   // Display post from  clicking Post (id) link
-  public function view($id = null) {
+  public function view($id) {
     if (!$id) {
       throw new NotFoundException(__('Invalid post'));
     }
@@ -25,13 +25,57 @@ class PostsController extends AppController {
 
   // Adding a post
   public function add() {
-    if ($this->requst->is('post')) {
+    if ($this->request->is('post')) {
       $this->Post->create();
       if ($this->Post->save($this->request->data)) {
         $this->Flash->success(__('Your post has been saved.'));
         return $this->redirect(array('action' => 'index'));
       }
-      $this->Flash->error(__('Unable to add your post.'))
+      $this->Flash->error(__('Unable to add your post.'));
     }
+  }
+
+  // Edit Post
+  public function edit($id = null) {
+    if (!$id) {
+      throw new NotFoundException(__('Invalid post'));
+    }
+
+    $post = $this->Post->findById($id);
+    if (!$post) {
+      throw new NotFoundException(__('Invalid post'));
+    }
+
+    if ($this->request->is(array('post', 'put'))) {
+      $this->Post->id = $id;
+      if ($this->Post->save($this->request->data)) {
+        $this->Flash->success(__('Your post has been updated.'));
+        return $this->redirect(array('action' => 'index'));
+      }
+      $this->Flash->error(__('Unable to update your post.'));
+    }
+
+    if (!$this->request->data) {
+      $this->request->data = $post;
+    }
+  }
+
+  // Delete Post
+  public function delete($id) {
+    if ($this->request->is('get')) {
+      throw new MethodNotAllowedException();
+    }
+
+    if ($this->Post->delete($id)) {
+      $this->Flash->success(
+        __('The post with id: %s has been deleted.', h($id))
+      );
+    } else {
+      $this->Flash->error(
+        __('The post with id: %s could not be deleted.', h($id))
+      );
+    }
+
+    return $this->redirect(array('action' => 'index'));
   }
 }
